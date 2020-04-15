7261f1a84e7273484d2d360b3fc151687ba82a79
Lina <lina.ahmedkamal@gmail.com>
2020-04-15 18:29:29 +0200

    submit submit date

--- js/scripts.js
+++ js/scripts.js
@@ -257,10 +257,12 @@
             // handle the invalid form...
             cformError();
             csubmitMSG(false, "Please fill all fields!");
+            console.log("error");
         } else {
             // everything looks good!
             event.preventDefault();
             csubmitForm();
+            console.log("Success");
         }
     });
 
@@ -384,3 +386,28 @@
 	});
 
 })(jQuery);
+
+
+
+
+function setInputDate(_id){
+    var _dat = document.querySelector(_id);
+    var hoy = new Date(),
+        d = hoy.getDate(),
+        m = hoy.getMonth()+1,
+        y = hoy.getFullYear(),
+        data;
+
+    if(d < 10){
+        d = "0"+d;
+    };
+    if(m < 10){
+        m = "0"+m;
+    };
+
+    data = y+"-"+m+"-"+d;
+    console.log(data);
+    _dat.value = data;
+};
+
+setInputDate("#submitDate");
